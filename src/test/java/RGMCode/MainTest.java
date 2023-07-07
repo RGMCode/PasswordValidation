72d144a024971393f007fb32fae77257b02c7e94
mrg <rgmengedoth@gmail.com>
2023-07-07 11:29:51 +0200

    done

--- src/test/java/RGMCode/MainTest.java
+++ src/test/java/RGMCode/MainTest.java
@@ -7,8 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void abcTest(){
+    void PasswordValidationTest(){
+        //given
+        String password = "neueFische2023";
+        //when
+        boolean actualBoolean = Main.pwCheck(password);
+        //then
+        assertTrue(actualBoolean);
 
     }
+
+    @Test
+    void PasswordValidationTestWeakPassword(){
+        //given
+        String password = "nF2023";
+        //when
+        //boolean actualBoolean = Main.pwCheck(password);
+        boolean actualBoolean = Main.passwordIsWeakPassword(password);
+        //then
+        assertFalse(actualBoolean);
+
+    }
+
 
 }
