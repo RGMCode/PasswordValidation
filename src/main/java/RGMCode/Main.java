0b1f86d1f5c64a5acd95b437912a7c4e01f36789
mrg <rgmengedoth@gmail.com>
2023-07-07 10:06:29 +0200

    first push

--- src/main/java/RGMCode/Main.java
+++ src/main/java/RGMCode/Main.java
@@ -5,7 +5,21 @@ public class Main {
 
     }
 
+/*
+Aufgabe: Passwortvalidierung
+Schreibe eine Passwortvalidierung.
 
+Lege dir ein neues Java Github Projekt an.
+Lass mittels CI die Tests automatisch laufen.
+Überlege dir, wie die Methoden aussehen müssen.
+Schreibe jeweils erst einen Test und dann die Funktionalität.
+Überprüfe erst die Länge des Passworts und
+dann, ob Zahlen enthalten sind.
+Bonus:
+
+Checke, ob kleine/große Buchstaben enthalten sind.
+Verbiete "schlechte" Passwörter wie "passwort" oder "123456".
+*/
 
 
 }
